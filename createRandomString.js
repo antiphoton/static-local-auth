2906069fd5b7e5a86502e4f9b5b823095ae9c77a
cbx <caoboxiao@gmail.com>
2018-06-21 11:51:07 -0400

    Fixed the bug that random string length was incorrect

--- createRandomString.js
+++ createRandomString.js
@@ -1,6 +1,6 @@
 'use strict';
 
 module.exports = function createRandomString(size) {
-  return Array(size).fill(0).map(() => Math.floor(Math.random() * 10)).join('');
+  return Array(size).fill(0).map(() => Math.floor(Math.random() * size)).join('');
 };
 
