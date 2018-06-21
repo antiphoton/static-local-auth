a31a061e824e3005a5f7cc41b7502ac4cbabfae6
cbx <caoboxiao@gmail.com>
2018-06-21 12:07:06 -0400

    Cache pug render result

--- writePage.js
+++ writePage.js
@@ -1,15 +1,23 @@
 'use strict';
 
+const path = require('path');
+
+const pug = require('pug');
+
 const cookieName = require('./cookieName');
 const queryName = require('./queryName');
 
+const html = pug.renderFile(
+  path.join(__dirname, 'page', 'index.pug'),
+  {
+    cookieName,
+    queryName,
+  },
+);
+
 module.exports = function writePage(res) {
-  res.render(
-    `${__dirname}/page/index.pug`,
-    {
-      cookieName,
-      queryName,
-    },
-  );
+  res.set('Content-Type', 'text/html');
+  res.send(html);
+  res.end();
 };
 
